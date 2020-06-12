e4b3384739d4f54cdb6631959040bdda0a184360
vasudha921 <shwethamoorthy@gmail.com>
2020-06-12 14:55:02 +0530

    created an synchronous ball game

--- sketch.js
+++ sketch.js
@@ -1,10 +1,16 @@
-var ball;
+var ball, database;
+var position; 
 
 function setup(){
+    database = firebase.database();
+    console.log(database);
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    var ballPosition = database.ref('ball/position');
+    ballPosition.on("value", readPosition, showError);
 }
+
 
 function draw(){
     background("white");
@@ -24,6 +30,18 @@ function draw(){
 }
 
 function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+    database.ref('ball/position').set({
+        'x': position.x+x,
+        'y': position.y+y 
+    })
+}
+
+function readPosition(data){
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;
+}
+
+function showError(){
+    console.log("Error.show reading");
 }
